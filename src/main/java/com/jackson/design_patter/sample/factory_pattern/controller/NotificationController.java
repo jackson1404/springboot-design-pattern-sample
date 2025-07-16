a9f0fe66144470503a287c41b6aec1dee064d768
jackson1404 <jackson14042004@gmail.com>
2025-07-16 23:34:29 +0630

    added notification controller for notify

--- src/main/java/com/jackson/design_patter/sample/factory_pattern/controller/NotificationController.java
+++ src/main/java/com/jackson/design_patter/sample/factory_pattern/controller/NotificationController.java
@@ -1,4 +1,27 @@
 package com.jackson.design_patter.sample.factory_pattern.controller;
 
+import com.jackson.design_patter.sample.factory_pattern.service.NotificationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/test")
 public class NotificationController {
+
+    private final NotificationService service;
+
+    public NotificationController(NotificationService service) {
+        this.service = service;
+    }
+
+    @PostMapping("/notify")
+    public String notify(@RequestParam String type,
+                         @RequestParam String to,
+                         @RequestParam String message) {
+        service.notify(type, to, message);
+        return "Notification sent via " + type;
+    }
 }
