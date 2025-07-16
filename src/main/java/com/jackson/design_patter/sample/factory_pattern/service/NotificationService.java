a6a93f115c1f6734285a1122bbf8352392b489e8
jackson1404 <jackson14042004@gmail.com>
2025-07-16 23:33:19 +0630

    added notification actual notify service

--- src/main/java/com/jackson/design_patter/sample/factory_pattern/service/NotificationService.java
+++ src/main/java/com/jackson/design_patter/sample/factory_pattern/service/NotificationService.java
@@ -1,4 +1,25 @@
 package com.jackson.design_patter.sample.factory_pattern.service;
 
+import com.jackson.design_patter.sample.factory_pattern.factory.NotificationSenderFactory;
+import com.jackson.design_patter.sample.factory_pattern.sender.NotificationSender;
+import org.springframework.stereotype.Service;
+
+@Service
 public class NotificationService {
+
+    private final NotificationSenderFactory factory;
+
+    public NotificationService(NotificationSenderFactory factory) {
+        this.factory = factory;
+    }
+
+    public void notify(String type, String to, String message) {
+        NotificationSender sender = factory.getSender(type);
+        if (sender != null) {
+            sender.send(to, message);
+        } else {
+            throw new IllegalArgumentException("Invalid notification type: " + type);
+        }
+    }
+
 }
