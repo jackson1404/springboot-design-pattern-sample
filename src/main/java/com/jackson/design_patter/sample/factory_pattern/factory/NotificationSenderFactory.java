e57f1bd066610d533f61011572a5d899bdac32b8
jackson1404 <jackson14042004@gmail.com>
2025-07-16 23:31:56 +0630

    added factory map sender method

--- src/main/java/com/jackson/design_patter/sample/factory_pattern/factory/NotificationSenderFactory.java
+++ src/main/java/com/jackson/design_patter/sample/factory_pattern/factory/NotificationSenderFactory.java
@@ -1,4 +1,27 @@
 package com.jackson.design_patter.sample.factory_pattern.factory;
 
+import com.jackson.design_patter.sample.factory_pattern.sender.NotificationSender;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+@Component
 public class NotificationSenderFactory {
+
+    private final Map<String, NotificationSender> notificationSenderMap;
+
+    public NotificationSenderFactory(List<NotificationSender> senderList) {
+        this.notificationSenderMap = senderList.stream()
+                .collect(Collectors.toMap(s -> s.getClass().getAnnotation(Component.class).value(), Function.identity()));
+    }
+
+    public NotificationSender getSender(String type) {
+        return notificationSenderMap.getOrDefault(type.toUpperCase(), null);
+    }
+
+
 }
