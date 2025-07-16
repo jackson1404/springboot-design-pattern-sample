28504ee66d430f402f40079a352204c8c5c46b0b
jackson1404 <jackson14042004@gmail.com>
2025-07-16 23:38:45 +0630

    added main method testing

--- src/main/java/com/jackson/design_patter/sample/Application.java
+++ src/main/java/com/jackson/design_patter/sample/Application.java
@@ -1,10 +1,21 @@
 package com.jackson.design_patter.sample;
 
+import com.jackson.design_patter.sample.factory_pattern.service.NotificationService;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
+
+	@Autowired
+	private NotificationService notificationService;
+
+	@PostConstruct
+	public void init(){
+		notificationService.notify("PUSH", "wmt", "hello");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
