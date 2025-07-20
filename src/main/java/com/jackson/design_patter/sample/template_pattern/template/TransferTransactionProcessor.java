4231b1d0b21df6dd109b260ea3e207e6f96e6372
jackson1404 <jackson14042004@gmail.com>
2025-07-20 19:41:58 +0630

    added transfer transaction method

--- src/main/java/com/jackson/design_patter/sample/template_pattern/template/TransferTransactionProcessor.java
+++ src/main/java/com/jackson/design_patter/sample/template_pattern/template/TransferTransactionProcessor.java
@@ -1,7 +1,35 @@
 package com.jackson.design_patter.sample.template_pattern.template;
 
+import com.jackson.design_patter.sample.template_pattern.model.TransactionEntity;
+import com.jackson.design_patter.sample.template_pattern.model.TransactionResult;
 import org.springframework.stereotype.Component;
 
 @Component
-public class TransferTransactionProcessor {
+public class TransferTransactionProcessor extends TransactionProcessorTemplate{
+
+    @Override
+    protected void checkBalance(TransactionEntity transactionEntity) {
+        System.out.println("Checking sufficient balance in source account for transfer: " +
+                transactionEntity.getSourceAccountId());
+
+    }
+
+    @Override
+    protected TransactionResult executeTransaction(TransactionEntity transactionEntity) {
+
+        System.out.println("Processing transfer from account: " + transactionEntity.getSourceAccountId() +
+                        "to " + transactionEntity.getTargetAccountId() +
+                        "amount " + transactionEntity.getAmount());
+
+        TransactionResult result = new TransactionResult();
+        result.setSuccess(true);
+        result.setMessage("Transfer completed successfully");
+        return result;
+    }
+
+    @Override
+    protected void notifyParties(TransactionEntity transactionEntity, TransactionResult result) {
+        super.notifyParties(transactionEntity, result);
+        System.out.println("Sending specific transfer notifications to both parties");
+    }
 }
