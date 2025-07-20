bff72929e0430220878f31f0c8fbff743410d17c
jackson1404 <jackson14042004@gmail.com>
2025-07-20 20:39:48 +0630

    added required withdrawal method

--- src/main/java/com/jackson/design_patter/sample/template_pattern/template/TransactionProcessorTemplate.java
+++ src/main/java/com/jackson/design_patter/sample/template_pattern/template/TransactionProcessorTemplate.java
@@ -54,7 +54,4 @@ public abstract class TransactionProcessorTemplate {
 
     }
 
-
-
-
 }
--- src/main/java/com/jackson/design_patter/sample/template_pattern/template/WithdrawalTransactionProcessor.java
+++ src/main/java/com/jackson/design_patter/sample/template_pattern/template/WithdrawalTransactionProcessor.java
@@ -1,7 +1,33 @@
 package com.jackson.design_patter.sample.template_pattern.template;
 
+import com.jackson.design_patter.sample.template_pattern.model.TransactionEntity;
+import com.jackson.design_patter.sample.template_pattern.model.TransactionResult;
 import org.springframework.stereotype.Component;
 
 @Component
-public class WithdrawalTransactionProcessor {
+public class WithdrawalTransactionProcessor extends TransactionProcessorTemplate{
+
+    @Override
+    protected void checkBalance(TransactionEntity transactionEntity) {
+        System.out.println("Checking balance for withdrawal transaction " +
+                transactionEntity.getSourceAccountId());
+    }
+
+    @Override
+    protected TransactionResult executeTransaction(TransactionEntity transactionEntity) {
+        System.out.println("Processing withdrawal from account: " +
+                transactionEntity.getSourceAccountId() +
+                " amount: " + transactionEntity.getAmount());
+
+        TransactionResult result = new TransactionResult();
+        result.setSuccess(true);
+        result.setMessage("Withdrawal completed successfully");
+        return result;
+    }
+
+    @Override
+    protected void notifyParties(TransactionEntity transaction, TransactionResult result) {
+        super.notifyParties(transaction, result);
+        System.out.println("Sending withdrawal alert to account holder");
+    }
 }
