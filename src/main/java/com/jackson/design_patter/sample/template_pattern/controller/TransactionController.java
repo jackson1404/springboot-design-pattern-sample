0a61da2ac9f9f2b9822f3f13d8e657cab14aeff2
jackson1404 <jackson14042004@gmail.com>
2025-07-20 21:35:01 +0630

    added transaction process service and get processor service

--- src/main/java/com/jackson/design_patter/sample/template_pattern/controller/TransactionController.java
+++ src/main/java/com/jackson/design_patter/sample/template_pattern/controller/TransactionController.java
@@ -1,9 +1,26 @@
 package com.jackson.design_patter.sample.template_pattern.controller;
 
+import com.jackson.design_patter.sample.template_pattern.model.TransactionEntity;
+import com.jackson.design_patter.sample.template_pattern.service.TransactionProcessingService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1")
+@RequiredArgsConstructor
 public class TransactionController {
+
+    private final TransactionProcessingService transactionProcessingService;
+
+//    @PostMapping("/transactions")
+//    public ResponseEntity<?> processTransactions(@RequestBody TransactionEntity transactionEntity){
+//        transactionProcessingService.
+//        return ResponseEntity.ok()
+//    }
 
 
 }
--- src/main/java/com/jackson/design_patter/sample/template_pattern/service/TransactionProcessingService.java
+++ src/main/java/com/jackson/design_patter/sample/template_pattern/service/TransactionProcessingService.java
@@ -1,7 +1,38 @@
 package com.jackson.design_patter.sample.template_pattern.service;
 
+import com.jackson.design_patter.sample.template_pattern.TransactionType;
+import com.jackson.design_patter.sample.template_pattern.model.TransactionEntity;
+import com.jackson.design_patter.sample.template_pattern.model.TransactionResult;
+import com.jackson.design_patter.sample.template_pattern.template.DepositTransactionProcessor;
+import com.jackson.design_patter.sample.template_pattern.template.TransactionProcessorTemplate;
+import com.jackson.design_patter.sample.template_pattern.template.TransferTransactionProcessor;
+import com.jackson.design_patter.sample.template_pattern.template.WithdrawalTransactionProcessor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class TransactionProcessingService {
+
+    private final DepositTransactionProcessor depositTransactionProcessor;
+    private final TransferTransactionProcessor transferTransactionProcessor;
+    private final WithdrawalTransactionProcessor withdrawalTransactionProcessor;
+
+    public TransactionResult processTransaction(TransactionEntity transactionEntity) throws IllegalAccessException {
+        TransactionProcessorTemplate processor = getProcessor(transactionEntity.getType());
+        return processor.processTransaction(transactionEntity);
+    }
+
+
+    private TransactionProcessorTemplate getProcessor(TransactionType transactionType) throws IllegalAccessException {
+        return switch (transactionType){
+            case DEPOSIT -> depositTransactionProcessor;
+            case WITHDRAWAL -> withdrawalTransactionProcessor;
+            case TRANSFER -> transferTransactionProcessor;
+            default -> throw new IllegalAccessException("Unknown transaction type " + transactionType);
+        };
+    }
+
+
+
 }
